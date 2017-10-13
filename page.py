169dff67fc1f6a0b1671ea037a1bb591b089b10a
YouhuiBai <youhuibai0108@gmail.com>
2017-10-13 08:06:52 +0800

    update

--- page.py
+++ page.py
@@ -1,5 +1,6 @@
 import threading
-workerNum = 4 # the total worker number in system
+workerNum = 4 # the number of worker in system
+logNum = 4 # the number of log in system
 
 # maxPage = 168955252 #the max page number in trace file
 maxPage = 131218 # the max page number in test trace file
