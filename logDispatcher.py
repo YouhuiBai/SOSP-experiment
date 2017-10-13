dca77ae1d97e14cb61192ef9dfa1c19a6bd6d15c
cheng <cheng@192.168.1.104>
2017-10-13 10:55:20 +0800

    test

--- logDispatcher.py
+++ logDispatcher.py
@@ -1,5 +1,12 @@
 import threading
 
+'''
+This file contains functions associated with
+LogDispatcher which is responsible for receiving
+log entries from txnExecutors, making plans for
+placing entries and sending plans to log handlers.
+'''
+
 class LogDispatcher :
 	Queue = []
 	mutex = threading.Lock()
